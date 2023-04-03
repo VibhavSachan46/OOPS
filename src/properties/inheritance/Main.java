0b0a38ba77fe0b7557bbe848e6d79f7873a07c4a
Vibhav <vibhavsachan743@gmail.com>
2023-04-04 01:38:39 +0530

    single level and multilevel inheritance

--- src/properties/inheritance/Main.java
+++ src/properties/inheritance/Main.java
@@ -4,20 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Box box1 = new Box(5.2);
-        System.out.println(box1.l +" " + box1.w + " " + box1.h);
-
-        Box box = new Box(box1);
-        System.out.println(box.l +" " + box.w + " " + box.h);
-
-        Boxweight box3 = new Boxweight();
-        System.out.println(box3.weight +" "+ box3.l +" " + box3.w + " " + box3.h);
+//        Box box1 = new Box(5.2);
+//        System.out.println(box1.l +" " + box1.w + " " + box1.h);
+//
+//        Box box = new Box(box1);
+//        System.out.println(box.l +" " + box.w + " " + box.h);
+//
+//        Boxweight box3 = new Boxweight();
+//        System.out.println(box3.weight +" "+ box3.l +" " + box3.w + " " + box3.h);
 
 
         //here type of object(Boxweight) will not determine what members will be access
         // but type of refernece will determine
-        Box box5 = new Boxweight(2,3,4,8);
-        System.out.println(box.w);
+//        Box box5 = new Boxweight(2,3,4,8);
+//        System.out.println(box.w);
 //        when a reference to a subclass object is assigned to super class
 //        variable only super class object will have access
 
